e647bc05ec4bcba02f24fe065cc4109f0ff815eb
Enrico Icardi <enrico.icardi@gmail.com>
2016-12-01 18:57:15 +0100

    small server refactoring

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
-app.use('/', express.static('_book')); // ← adjust
-app.listen(port, function() { console.log('listening'); });
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/_book'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
